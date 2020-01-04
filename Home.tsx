e7e5f87c2252873440630495ff592a7f5e6d92dc
tran <tran.lee@outlook.com>
2020-01-04 15:52:10 -0500

    style changes

--- Home.tsx
+++ Home.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { View, Text, Image, Button } from "react-native";
+import { View, Text, Image, Button, StyleSheet } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
 
 interface HomeScreenProps {
   navigation;
@@ -13,27 +14,74 @@ export default class HomeScreen extends Component<HomeScreenProps> {
   }
   render() {
     return (
-      <View
-        style={{
-          flex: 1,
-          flexDirection: "column",
-          justifyContent: "flex-start",
-          alignContent: "space-around",
-          paddingTop: 50,
-          paddingLeft: 50
-        }}
-      >
-        <Image style={{ width: 70, height: 70 }} source={{ uri: LOGO }}></Image>
-        <Text>BLENDR</Text>
-        <Button
-          title="Sign in"
-          onPress={() => this.props.navigation.navigate("LoginScreen")}
-        />
-        <Button
-          title="Register"
-          onPress={() => this.props.navigation.navigate("RegisterScreen")}
-        />
+      <View style={styles.container}>
+        <View style={styles.logoContainer}>
+          <Image style={styles.logo} source={{ uri: LOGO }} />
+          <Text style={styles.title} />
+        </View>
+
+        <View style={styles.formContainer}>
+          <TextInput style={styles.input} />
+
+          <TextInput
+            placeholder="Usename"
+            placeholderTextColor="Dark Grey"
+            style={styles.input}
+          />
+
+          <TextInput placeholder="Password" style={styles.input} />
+          <View style={styles.buttonSigin}>
+            <Button title="the button">
+              <Text style={styles.buttonText}>Sign In</Text>
+            </Button>
+          </View>
+        </View>
       </View>
     );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white"
+  },
+
+  logo: {
+    width: 100,
+    heigth: 140
+  },
+
+  logoContainer: {
+    alignItems: "center",
+    flexGrow: 1,
+    justifyContent: "center"
+  },
+
+  formContainer: {
+    padding: 20
+  },
+
+  input: {
+    height: 40,
+    backgroundColor: "green",
+    marginBottom: 20,
+    color: "black"
+  },
+
+  title: {
+    color: "dark grey",
+    marginTop: 25,
+    textAlign: "center"
+  },
+
+  buttonSigin: {
+    backgroundColor: "green",
+    paddingVertical: 37
+  },
+
+  buttonText: {
+    textAlign: "center",
+    color: "dark grey",
+    fontWeight: "700"
+  }
+});
