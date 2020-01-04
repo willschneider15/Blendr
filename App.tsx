ee3a228f1d2e9aaa13e80422f152f43b09593b0c
Joey <sjferenchak@gmail.com>
2020-01-03 22:54:44 -0500

    added home file to be worked on

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './Home';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <HomeScreen />
     </View>
   );
 }
